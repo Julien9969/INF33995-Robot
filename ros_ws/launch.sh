69953b2abcdf3f18a6a3cff41799425d262c68da
Erreur-404 <etienne-2.perron@polymtl.ca>
2024-02-08 12:42:46 -0500

    Remove unnecessary scripts

--- ros_ws/launch.sh
+++
@@ -1,3 +0,0 @@
-colcon build --cmake-args -DBUILD_TESTING=ON
-source install/setup.sh
-ros2 launch py_identify_server identify.py
--- ros_ws/start.sh
+++
@@ -1,4 +0,0 @@
-apt update
-rosdep init
-rosdep update
-rosdep install --from-paths src --ignore-src -r -i -y --rosdistro $ROS_DISTRO
