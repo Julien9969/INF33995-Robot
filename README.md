ecb5475114bfde4a23b4b8241c54482fd5c75e7f
Erreur-404 <etienne-2.perron@polymtl.ca>
2024-02-11 17:42:41 -0500

    Add script to automate deployment and namespacing of robots

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ### Robot
 
-Pour rouler le docker du robot, utiliser le fichier `robot/docker-compose.yml`. Celui-ci permet au docker d'avoir accès aux périphériques du robot.
+Pour rouler le docker du robot, il suffit de lancer le script `robot/launch-robot.sh` et de rentrer le mot de passe de l'utilisateur nvidia sur le robot lorsque demandé. Le script prend en paramètre l'adresse ip des robots qu'on souhaite lancer ainsi que la branch du dépot git sur laquelle on souhaite que les robots se synchronise. Par défaut, les adresses ip de nos robots sont prises en compte et la branche main est choisie.
+
+Si une erreur survient ou si pour tout autre raison vous souhaitez ne pas utiliser ce script, vous pouvez vous connecter directement au robot, cloner le dépot si ce n'est pas déjà fait, naviguer à `INF3995-Robot/robot/` et, modifier la valeur de la variable d'environnement `ROBOT_NUM` du fichier `docker-compose.yml` afin que celle du robot courant soit unique, puis lancer le robot avec `docker compose up`.
 
 ### Simulation
 
---
+++ robot/launch-robot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+BRANCH="main"
+ROBOTS=("192.168.0.102" "192.168.0.106")
+USERNAME=nvidia
+
+function print_usage {
+    echo "Usage: ./launch-robot.sh [-b|--branch <branch-name>] [-r|--robots <ip-addresses-of-robots-separated-by-space>] [-h|--help]"
+    exit 1
+}
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -b|--branch)
+            BRANCH="$2"
+            shift
+            shift
+            ;;
+        -r|--robots)
+            ROBOTS=()
+            shift
+            # Parse until the next option or end of ip addresses
+            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+                ROBOTS+=("$1")
+                shift
+            done
+            ;;
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            print_usage
+            exit 1
+            ;;
+    esac
+done
+
+NUM_ROBOTS=${#ROBOTS[@]}
+
+for i in $(seq 1 $NUM_ROBOTS);
+do
+    ssh $USERNAME@${ROBOTS[$((i-1))]}  """
+        if [ -d /home/nvidia/INF3995-Robot ]; then
+            cd /home/nvidia/INF3995-Robot && git pull && git switch $BRANCH && git pull;
+        else
+            git clone git@gitlab.com:polytechnique-montr-al/inf3995/20241/equipe-102/INF3995-Robot.git;
+        fi
+        sed -i 's/ROBOT_NUM=[a-zA-Z0-9]*/ROBOT_NUM='$i'/g' /home/nvidia/INF3995-Robot/robot/docker-compose.yml
+        cd /home/nvidia/INF3995-Robot/robot && docker compose up
+        """
+done
