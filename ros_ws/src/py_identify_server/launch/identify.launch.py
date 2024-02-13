07c1279edc3ae9413dc3534b2ed818027e73082b
Erreur-404 <etienne-2.perron@polymtl.ca>
2024-02-13 12:48:39 -0500

    Add template namespace to launch file

--- ros_ws/src/py_identify_server/launch/identify.launch.py
+++ ros_ws/src/py_identify_server/launch/identify.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
     return LaunchDescription([
@@ -7,5 +8,6 @@ def generate_launch_description():
             package='py_identify_server',
             executable='identify',
             output='screen',
+            namespace=f'robot{os.environ["ROBOT_NUM"]}',
         ),
     ])
