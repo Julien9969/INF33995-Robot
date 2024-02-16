5f9cf7e7801355153bfab59898ae2f033f507751
Majeed ABaki <majeed.abdul-baki@polymtl.ca>
2024-02-16 01:27:57 +0000

    Hotfix/robot package import

--- ros_ws/src/py_identify_server/py_identify_server/identify.py
+++ ros_ws/src/py_identify_server/py_identify_server/identify.py
@@ -1,10 +1,11 @@
 import time
 from interfaces.srv import Identify
+import os
 
 import sys
 import rclpy
 from rclpy.node import Node
-from .spin_robot import SpinRobot
+from geometry_msgs.msg import Twist
 
 
 class IdentifyService(Node):
@@ -12,16 +13,22 @@ class IdentifyService(Node):
     def __init__(self):
         super().__init__('identify_service')
         self.srv = self.create_service(Identify, 'identify', self.serve)
+        self.publisher_ = self.create_publisher(Twist, f'/robot{os.environ["ROBOT_NUM"]}/cmd_vel', 10)
 
     def serve(self, request, response):
         response.b = request.a * 2
         self.get_logger().info(f'Incoming request, a: {request.a}')
 
-        SpinRobot().trigger()
-        time.sleep(10)
-        SpinRobot().stop()
+        for i in range(5):
+            self.trigger()
+            time.sleep(0.5)
 
         return response
+
+    def trigger(self):
+        rotate_msg = Twist()
+        rotate_msg.angular.z = 3.14
+        self.publisher_.publish(rotate_msg)
 
 
 def main():
