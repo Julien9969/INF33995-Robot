68ffa9d4baf57c962655e5d563f06cdec5a4e12d
Erreur-404 <etienne-2.perron@polymtl.ca>
2024-02-29 10:20:27 -0500

    Make ros processes run in background

--- robot/rebuild-robot.sh
+++ robot/rebuild-robot.sh
@@ -4,7 +4,7 @@ killall ros2
 cd /home/nvidia/INF3995-Robot/ros_ws/ && rm -rf /home/nvidia/INF3995-Robot/ros_ws/build/ /home/nvidia/INF3995-Robot/ros_ws/install/ /home/nvidia/INF3995-Robot/ros_ws/log/
 colcon build
 source /home/nvidia/INF3995-Robot/ros_ws/install/setup.bash
-ros2 launch robot_bringup robot_bringup.launch.py
+ros2 launch robot_bringup robot_bringup.launch.py &
 
 source /home/nvidia/INF3995-Robot/file_transfer_ws/install/setup.bash
-ros2 launch file_server robot_bringup.launch.py
+ros2 launch file_server robot_bringup.launch.py &
