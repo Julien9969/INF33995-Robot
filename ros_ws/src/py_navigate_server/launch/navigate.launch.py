3c1a411c326e96e7f9bf1df655ce9fe51f798054
VictorGilbertP <zevictos@gmail.com>
2024-03-05 16:03:09 -0500

    j'ai rajoute amcl dasns les node a commencer au launch de navigationn

--- ros_ws/src/py_navigate_server/launch/navigate.launch.py
+++ ros_ws/src/py_navigate_server/launch/navigate.launch.py
@@ -5,6 +5,12 @@ import os
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='nav2_amcl',
+            executable='amcl',
+            output='screen',
+            namespace=f'robot{os.environ["ROBOT_NUM"]}',
+        ),
+        Node(
             package='py_navigate_server',
             executable='navigate',
             output='screen',
