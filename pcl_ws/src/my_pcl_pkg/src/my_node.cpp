a1ac12ca1162599da14d1ec168a942de30428a7a
MajeedAB <majeedbaki@outlook.com>
2024-03-15 16:13:24 -0400

    OKAY WE GET POINT CLOUD DUBSCRIBER, NEXT IS INTERPRETING

--- pcl_ws/src/my_pcl_pkg/src/my_node.cpp
+++ pcl_ws/src/my_pcl_pkg/src/my_node.cpp
@@ -1,12 +1,42 @@
 #include <cstdio>
-#include "convert.cpp"
+#include "my_pcl_pkg/convert.h"
+
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("my_node")
+    {
+      subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      "rgbd/image_raw/points", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const sensor_msgs::msg::PointCloud2 & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%u'", msg.data);
+      
+      cloud_callback(msg);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+};
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world my_pcl_pkg package\n");
-  cloud_callback(nullptr);
+
+  printf("STARTING NODE PCL\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
   return 0;
+
 }
