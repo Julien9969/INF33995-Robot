30d99d206614ec563d28eec83f5f844a0e0c7d59
nvidia <nvidia@majeed.com>
2024-03-19 12:47:00 -0400

    Battery level now available

--- ros_ws/src/limo_info/limo_info/publisher.py
+++ ros_ws/src/limo_info/limo_info/publisher.py
@@ -1,20 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from interfaces.msg import LimoInfo, LimoStatus
+from limo_msgs.msg import LimoStatus
+from interfaces.msg import LimoInfo
 
 class LimoInfoPublisher(Node):
 
     def __init__(self):
         super().__init__('limo_info_publisher')
-        self.battery_level_percentage_ = 12
+        self.battery_level_percentage_ = 100
 
         self.publisher_ = self.create_publisher(LimoInfo, 'limo_info', 10)
-        self.limo_status_subscriber_ = self.create_subscription(LimoStatus, 'limo_status', self.limo_status_listener_callback, 10)
+        self.limo_status_subscriber_ = self.create_subscription(LimoStatus, '/robot2/limo_status', self.limo_status_listener_callback, 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def limo_status_listener_callback(self, msg):
-        self.battery_level_percentage_ = msg.battery_voltage / 12 * 100
+        self.battery_level_percentage_ = int((msg.battery_voltage - 9) / 3 * 100)
 
     def timer_callback(self):
         msg = LimoInfo()
