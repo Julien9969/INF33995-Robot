f12fbcd7b61b7ace85168bcce69ee6aa9de60a76
Trizotto <julien.roux404@gmail.com>
2024-03-19 12:41:26 -0400

    chore: restore robot

--- ros_ws/src/mission_control/mission_control/mission_switch_server.py
+++ ros_ws/src/mission_control/mission_control/mission_switch_server.py
@@ -22,7 +22,7 @@ class MissionSwitchService(Node):
         super().__init__('mission_switch')
         # TODO change the topic name
 
-        self.publisher_ = self.create_publisher(Twist, f'robot1/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, f'cmd_vel', 10)
         self.timer= self.create_timer(0.5, self.timer_callback)
         self.srv = self.create_service(MissionSwitch, 'mission_switch', self.serve)
         self.state = State.OFF
