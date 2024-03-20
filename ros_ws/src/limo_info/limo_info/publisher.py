36a27e78c944c130f45a76bd12fad32e092f44c7
Erreur-404 <etienne-2.perron@polymtl.ca>
2024-03-20 17:05:24 -0400

    Add battery management for simulation

--- ros_ws/src/limo_info/limo_info/publisher.py
+++ ros_ws/src/limo_info/limo_info/publisher.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from limo_msgs.msg import LimoStatus
-from interfaces.msg import LimoInfo
 import os
 
 class LimoInfoPublisher(Node):
@@ -10,20 +9,26 @@ class LimoInfoPublisher(Node):
         super().__init__('limo_info_publisher')
         self.battery_level_percentage_ = 100
 
-        robot_num = os.environ['ROBOT_NUM']
-        self.publisher_ = self.create_publisher(LimoInfo, 'limo_info', 10)
-        self.limo_status_subscriber_ = self.create_subscription(LimoStatus, (f'/robot{robot_num}/limo_status'), self.limo_status_listener_callback, 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        if 'ROBOT_NUM' not in os.environ:
+            timer_period = 20  # seconds
+            self.timer = self.create_timer(timer_period, self.simulation_timer_callback)
+        else:
+            robot_num = os.environ['ROBOT_NUM']
+            self.limo_status_subscriber_ = self.create_subscription(LimoStatus, (f'/robot{robot_num}/limo_status'), self.limo_status_listener_callback, 10)
+            timer_period = 0.5  # seconds
+            self.timer = self.create_timer(timer_period, self.robot_timer_callback)
 
     def limo_status_listener_callback(self, msg):
         self.battery_level_percentage_ = int((msg.battery_voltage - 9) / 3 * 100)
 
-    def timer_callback(self):
-        msg = LimoInfo()
-        msg.battery_level_percentage = self.battery_level_percentage_
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: \n\tBattery Level (%): {msg.battery_level_percentage}%')
+    def simulation_timer_callback(self):
+        self.battery_level_percentage_ -= 1
+        if self.battery_level_percentage_ < 0:
+            self.battery_level_percentage_ = 0
+        self.get_logger().info(f'BATTERY: {self.battery_level_percentage_}%')
+
+    def robot_timer_callback(self):
+        self.get_logger().info(f'BATTERY: {self.battery_level_percentage_}%')
 
 
 def main(args=None):
