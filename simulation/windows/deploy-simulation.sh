3e4c2fc7476fc9891f633017484c2d192a7fe7d7
MajeedAB <majeedbaki@outlook.com>
2024-03-23 21:49:06 -0400

    window deploy script fix too hohoho

--- simulation/windows/deploy-simulation.sh
+++ simulation/windows/deploy-simulation.sh
@@ -1,5 +1,12 @@
 apt update
-cd INF3995-Robot/ros_ws
+mkdir -p "/usr/local/include/" && cp /opt/ros/humble/include/tf2_geometry_msgs/tf2_geometry_msgs/ /usr/local/include/ -r
+apt update
+rm -r /root/INF3995-Robot/simulation/.venv
+apt install python3.10-venv -y
+cd /root/INF3995-Robot/simulation && python3 -m venv .venv
+source /root/INF3995-Robot/simulation/.venv/bin/activate && pip install python-sdformat catkin_pkg empy==3.3.4 && pip uninstall em && pip3 install lark --prefer-binary && python3 /root/INF3995-Robot/simulation/random_map_generator.py
+
+cd /root/INF3995-Robot/ros_ws
 rm /etc/ros/rosdep/sources.list.d/20-default.list
 rosdep init
 rosdep update
