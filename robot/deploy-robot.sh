197600c17bd5d9edfa20add221b98c9078fb3487
Trizotto <julien.roux404@gmail.com>
2024-04-16 17:03:14 -0400

    clean script

--- robot/deploy-robot.sh
+++ robot/deploy-robot.sh
@@ -5,19 +5,8 @@ for process in "${processes[@]}"; do
     killall -9 $process
 done
 
-# apt update -y
-
 echo "export ROBOT_ENV='ROBOT'" >> /home/nvidia/.bashrc
 
-# rm -rf /home/nvidia/INF3995-Robot/file_transfer_ws/build/ /home/nvidia/INF3995-Robot/file_transfer_ws/install/ /home/nvidia/INF3995-Robot/file_transfer_ws/log/
-# rm -rf /home/nvidia/INF3995-Robot/ros_ws/build/ /home/nvidia/INF3995-Robot/ros_ws/install/ /home/nvidia/INF3995-Robot/ros_ws/log/
-
-# rm /etc/ros/rosdep/sources.list.d/20-default.list
-# rosdep init
-# rosdep update
-
-# rosdep install --from-paths /home/nvidia/INF3995-Robot/ros_ws/src --ignore-src -r -i -y --rosdistro humble
-# rosdep install --from-paths /home/nvidia/INF3995-Robot/file_transfer_ws/src --ignore-src -r -i -y --rosdistro humble
 sleep 3
 
 cd /home/nvidia/INF3995-Robot/ros_ws
